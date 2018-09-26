b543defa00b4e734b3244c0812e53f083591e4f8
shtolce <kimdimka@yandex.ru>
2018-09-26 23:30:36 +1000

    1

---
+++ src/Game.js
@@ -0,0 +1,13 @@
+Ball.Game = function(game) {};
+Ball.Game.prototype = {
+    create: function() {},
+    initLevels: function() {},
+    showLevel: function(level) {},
+    updateCounter: function() {},
+    managePause: function() {},
+    manageAudio: function() {},
+    update: function() {},
+    wallCollision: function() {},
+    handleOrientation: function(e) {},
+    finishLevel: function() {}
+};
