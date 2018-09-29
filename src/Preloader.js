310db0b9fc7db192ee2c73f2225895e5fa5d3bbe
shtolce <kimdimka@yandex.ru>
2018-09-29 13:19:30 +1000

    111

--- src/Preloader.js
+++ src/Preloader.js
@@ -1,10 +1,11 @@
-Ball.Preloader=function(game){};
-Ball.Preloader.prototype={
-    preload:function(){
-        this.preloadBg=this.add.sprite( (Ball._WIDTH-297)*0.5,(Ball._HEIGHT-145)*0.5,'preloaderBg' );
-        this.preloadBar=this.add.sprite( (Ball._WIDTH-158)*0.5,(Ball._HEIGHT-50)*0.5,'preloaderBar' );
-        this.load.setPreloadSprite(this.preloadBar);
-        this.load.image('ball', 'img/ball.png');
+Ball.Preloader = function(game) {};
+Ball.Preloader.prototype = {
+	preload: function() {
+		this.preloadBg = this.add.sprite((Ball._WIDTH-297)*0.5, (Ball._HEIGHT-145)*0.5, 'preloaderBg');
+		this.preloadBar = this.add.sprite((Ball._WIDTH-158)*0.5, (Ball._HEIGHT-50)*0.5, 'preloaderBar');
+		this.load.setPreloadSprite(this.preloadBar);
+
+		this.load.image('ball', 'img/ball.png');
 		this.load.image('hole', 'img/hole.png');
 		this.load.image('element-w', 'img/element-w.png');
 		this.load.image('element-h', 'img/element-h.png');
@@ -21,9 +22,8 @@ Ball.Preloader.prototype={
 		this.load.spritesheet('button-start', 'img/button-start.png', 146, 51);
 
 		this.load.audio('audio-bounce', ['audio/bounce.ogg', 'audio/bounce.mp3', 'audio/bounce.m4a']);
-   },
-    create:function(){
-        this.game.state.start('MainMenu');
-    }
-
+	},
+	create: function() {
+		this.game.state.start('MainMenu');
+	}
 };
